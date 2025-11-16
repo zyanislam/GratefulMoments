5f681e7d84f01a9763d8bb866dc938f9e5e6fde7
Zyan <sheikh.zyanislam@gmail.com>
2025-11-16 18:20:11 +0600

    Added functional photo picker

---
+++ GratefulMoments/Models/Moment.swift
@@ -0,0 +1,8 @@
+//
+//  Moment.swift
+//  GratefulMoments
+//
+//  Created by Sheikh Fahad A. Islam on 16/11/25.
+//
+
+import Foundation
--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -11,6 +11,8 @@ import PhotosUI
 struct MomentEntryView: View {
     @State private var title = ""
     @State private var note = ""
+    @State private var imageData: Data?
+    @State private var newImage: PhotosPickerItem?
 
     var body: some View {
         NavigationStack{
@@ -22,13 +24,29 @@ struct MomentEntryView: View {
         }
     }
     
-    private var photoPicker: some View{
-        Image(systemName: "photo.badge.plus.fill")
-            .font(.largeTitle)
-            .frame(height: 250)
-            .frame(maxWidth: .infinity)
-            .background(Color(white: 0.4, opacity: 0.32))
+    private var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group {
+                if let imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                }
+            }
             .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: newImage) {
+            guard let newImage else { return }
+            Task {
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
+        }
     }
     
     var contentStack: some View {
